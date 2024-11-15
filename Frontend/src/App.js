56c794e3b4197f838bf97a1f27208f7ac10089e9
Lokeshkale2000 <lokesh.21910292@viit.ac.in>
2024-11-15 18:44:16 +0530

    first commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,106 @@
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from "./Components/home/Header";
+import Home from "./Components/home/Home";
+import Login from "./Components/home/Login";
+import Signup from "./Components/home/Signup";
+import Resume from "./Components/home/Resume";
+import ForgotPassword from "./Components/home/ForgotPassword";
+import OtpVerification from "./Components/home/OtpVerification";
+import ResumeFile from "./Components/home/ResumeFile";
+import FAQ from "./Components/home/FAQ";
+import ZetyFile from "./Components/home/ZetyFile";
+import Createresume from "./Components/home/Createresume";
+//import Footer from "./Components/Footer";
+import PrivacySettings from "./Components/pages/Account Setting/Privacy Setting/Privacy";
+//import Slidenavbar from './Sildenavbar/Slidenavbar';
+import GeneralAccountSettings from "./Components/pages/Account Setting/General Account/General";
+import Subscription from "./Components/pages/Account Setting/Subscription/Subscription";
+import CommunicationPreferences from "./Components/pages/Account Setting/Communication/Communication";
+import ContactForm from "./Components/coverletter/ContactForm";
+import RecipientForm from "./Components/coverletter/RecipientForm";
+import Subject from "./Components/coverletter/Subject";
+import Letterbody from "./Components/coverletter/Letterbody";
+import Opening from "./Components/coverletter/Opening";
+import Conclusion from "./Components/coverletter/Conclusion";
+import TemplateManager from "./Components/pages/templateManages/TemplateManager";
+import TemplateDetails1 from "./Components/coverlettercomponent/CoverletterTemp1";
+import TemplateDetails2 from "./Components/coverlettercomponent/CoverletterTemp2";
+import TemplateDetails3 from "./Components/coverlettercomponent/CoverletterTemp3";
+import TemplateDetails4 from "./Components/coverlettercomponent/CoverletterTemp4";
+import Upload from "./Components/pages/Upload/Upload";
+import PaymentUi from "./Components/pages/Payment/PaymentUi/PaymentUi";
+import HomePage from "./pages/HomePage";
+import ExperiencePage from "./pages/ExperiencePage";
+import StudentPage from "./pages/StudentPage";
+import EducationPage from "./pages/EducationPage";
+import TemplatePage from "./pages/TemplatePage";
+import SelectResumePage from "./pages/SelectResumePage";
+import UploadResumePage from "./pages/UploadResumePage";
+import ResumeBuilder from "./resume/ResumeBuilder";
+import Pricing from "./Components/home/Pricing";
+
+function App() {
+  const location = useLocation(); // Get the current route path
+
+  // Add '/forgot-password' to the list of routes where Header is hidden
+  const hideHeaderRoutes = ["/login", "/signup", "/resume", "/forgot-password"];
+
+  return (
+    <>
+      {/* Conditionally render Header based on the current path */}
+      {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/otp-verification" element={<OtpVerification />} />
+        <Route path="/resumefile" element={<ResumeFile />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/zety" element={<ZetyFile />} />
+        <Route path="/create-resume" element={<Createresume />} />
+        <Route
+          path="/account-settings/general"
+          element={<GeneralAccountSettings />}
+        />
+        <Route
+          path="/account-settings/subscription"
+          element={<Subscription />}
+        />
+        <Route
+          path="/account-settings/communication"
+          element={<CommunicationPreferences />}
+        />
+        <Route path="/account-settings/privacy" element={<PrivacySettings />} />
+        <Route path="/cover-letter" element={<ContactForm />} />
+        <Route path="/recipient-form" element={<RecipientForm />} />
+        <Route path="/subject" element={<Subject />} />
+        <Route path="/letterbody-page" element={<Letterbody />} />
+        <Route path="/opening-page" element={<Opening />} />
+
+        <Route path="/closing-page" element={<Conclusion />} />
+        <Route path="/payment" element={<PaymentUi />} />
+        <Route path="/tempmanager" element={<TemplateManager />}></Route>
+        <Route path="/template/1" element={<TemplateDetails1 />} />
+        <Route path="/template/2" element={<TemplateDetails2 />} />
+        <Route path="/template/3" element={<TemplateDetails3 />} />
+        <Route path="/template/4" element={<TemplateDetails4 />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/experience" element={<ExperiencePage />} />
+        <Route path="/student" element={<StudentPage />} />
+        <Route path="/education" element={<EducationPage />} />
+        <Route path="/templates" element={<TemplatePage />} />
+        <Route path="/select-resume" element={<SelectResumePage />} />
+        <Route path="/upload-resume" element={<UploadResumePage />} />
+        <Route path="/resume-editing" element={<ResumeBuilder />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Components/home/Header.js
@@ -0,0 +1,223 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  const [showToolsCard, setShowToolsCard] = useState(false);
+  const [showResume, setShowResume] = useState(false);
+  const [showCv, setShowCv] = useState(false);
+  const [showLetter, setShowLetter] = useState(false);
+  const [showBlog, setShowBlog] = useState(false);
+  const [showAbout, setShowAbout] = useState(false);
+  const navigate = useNavigate();
+
+ 
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  };
+
+  const toggleToolsCard = () => {
+    setShowToolsCard((prevState) => !prevState);
+  };
+
+  const toggleResumeCard = () => {
+    setShowResume((prevState) => !prevState);
+  };
+
+  const toggleCvCard = () => {
+    setShowCv((prevState) => !prevState);
+  };
+
+  const toggleCoverLetterCard = () => {
+    setShowLetter((prevState) => !prevState);
+  };
+
+  const toggleBlogCard = () => {
+    setShowBlog((prevState) => !prevState);
+  };
+
+  const toggleAboutCard = () => {
+    setShowAbout((prevState) => !prevState);
+  };
+
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <div className="nav-left"></div>
+        <div className="nav-links">
+          {/* Tools Dropdown */}
+       
+          {/* Resume Dropdown */}
+          <div className="nav-item">
+            <span className="nav-link" onClick={toggleResumeCard}>
+              Resume <span className="dropdown-icon">▼</span>
+            </span>
+            {showResume && (
+              <div className="tools-card">
+                <div className="tool-item">
+                  <img src="coverletter.png" alt="Resume Builder" />
+                  <div>
+                    <strong>Resume Builder</strong>
+                    <p>Create a resume in 5 minutes. Get the job you want.</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="tool-item">
+                  <img src="download (1).png" alt="Resume Checker" />
+                  <div>
+                    <strong>Resume Checker</strong>
+                    <p>Get your resume checked and scored with one click.</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="tool-item">
+                  <img src="download (1).png" alt="CV Maker" />
+                  <div>
+                    <strong>CV Maker</strong>
+                    <p>Create a CV in 5 minutes. Get the job you want.</p>
+                  </div>
+                </div>
+                <hr />
+                <div className="tool-item">
+                  <img src="download (1).png" alt="Cover Letter Builder" />
+                  <div>
+                    <strong>Cover Letter Builder</strong>
+                    <p>
+                      Write a cover letter that convinces employers you're the
+                      best.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+
+          {/* CV Dropdown */}
+          <div className="nav-item">
+            <span className="nav-link" onClick={toggleCvCard}>
+              CV <span className="dropdown-icon">▼</span>
+            </span>
+            {showCv && (
+              <div className="tools-card">
+              <div className="tool-item" onClick={() => handleNavigate("/homepage")}>
+                <img src="coverletter.png" alt="CV Builder" />
+                <div>
+                  <strong>CV Builder</strong>
+                  <p>Create a CV in 5 minutes. Get the job you want.</p>
+                </div>
+              </div>
+              <hr />
+              <div className="tool-item" onClick={() => handleNavigate("/cv-examples")}>
+                <img src="download (1).png" alt="CV Examples" />
+                <div>
+                  <strong>CV Examples</strong>
+                  <p>See perfect CV samples that get jobs.</p>
+                </div>
+              </div>
+              <hr />
+              <div className="tool-item" onClick={() => handleNavigate("/cv-help")}>
+                <img src="download (1).png" alt="CV Help" />
+                <div>
+                  <strong>CV Help</strong>
+                  <p>Improve your CV with help from expert guides.</p>
+                </div>
+              </div>
+            </div>
+            
+            )}
+          </div>
+
+    
+          <div className="nav-item">
+      <span className="nav-link" onClick={toggleCoverLetterCard}>
+        Cover Letter <span className="dropdown-icon">▼</span>
+      </span>
+      {showLetter && (
+        <div className="tools-card">
+          <div className="tool-item" onClick={() => handleNavigate("/upload")}>
+            <img src="coverletter.png" alt="Cover Letter Builder" />
+            <div>
+              <strong>Cover Letter Builder</strong>
+              <p>Create a cover letter in 5 minutes. Get the job you want.</p>
+            </div>
+          </div>
+          <hr />
+          <div className="tool-item" onClick={() => handleNavigate("/tempmanager")}>
+            <img src="download (1).png" alt="Cover Letter Templates" />
+            <div>
+              <strong>Cover Letter Templates</strong>
+              <p>Find the perfect cover letter template.</p>
+            </div>
+          </div>
+          <hr />
+          <div className="tool-item" onClick={() => handleNavigate("/tempmanager")}>
+            <img src="download (1).png" alt="Cover Letter Examples" />
+            <div>
+              <strong>Cover Letter Examples</strong>
+              <p>See perfect cover letter samples that get jobs.</p>
+            </div>
+          </div>
+
+          <button type="button" onClick={() => handleNavigate("/create-cover-letter")}>
+            Create Your Cover Letter Now
+          </button>
+        </div>
+      )}
+    </div>
+          {/* Blog Dropdown */}
+          <div className="nav-item">
+            <span className="nav-link" onClick={toggleBlogCard}>
+              Pricing  <span className="dropdown-icon">▼</span>
+            </span>
+            {showBlog && (
+             <div className="tools-card">
+             <div className="tool-item" onClick={() => handleNavigate("/pricing")}>
+               <img src="coverletter.png" alt="Latest Blog" />
+               <div>
+                 <strong>Pricing offer</strong>
+                 <p>Check out our latest blog posts on job hunting.</p>
+               </div>
+             </div>
+             <hr />
+             <div className="tool-item" onClick={() => handleNavigate("/payment")}>
+               <img src="download (1).png" alt="Blog Categories" />
+               <div>
+                 <strong>Payment</strong>
+                 <p>Explore various categories for your job search.</p>
+               </div>
+                </div>
+             
+              </div>
+            )}
+          </div>
+
+          {/* About Dropdown */}
+          <div className="nav-item">
+            <span className="nav-link" onClick={toggleAboutCard}>
+            Setting <span className="dropdown-icon">▼</span>
+            </span>
+            {showAbout && (
+              <div className="tools-card">
+             
+                <div className="tool-item">
+                  <img src="download (1).png" alt="Contact" />
+                  <div>
+                    <strong>Account Setting</strong>
+                    <p>Get in touch with us for any inquiries.</p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <Link to="/login" className="account-button">
+          My Account
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
